5bf14cd04cfd455b1b773ef462fedd8247648133
HotariTobu <hotari24tools@gmail.com>
2023-11-04 09:36:08 +0900

    first commit

---
+++ PI/MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace PI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.StartButton = new System.Windows.Forms.Button();
+            this.Ticker = new System.Windows.Forms.Timer(this.components);
+            this.StopButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ExponentBox = new System.Windows.Forms.TextBox();
+            this.ProgressStatusBar = new System.Windows.Forms.ProgressBar();
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(12, 45);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // Ticker
+            // 
+            this.Ticker.Tick += new System.EventHandler(this.Ticker_Tick);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Enabled = false;
+            this.StopButton.Location = new System.Drawing.Point(93, 45);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "2";
+            // 
+            // ExponentBox
+            // 
+            this.ExponentBox.BackColor = System.Drawing.SystemColors.Control;
+            this.ExponentBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ExponentBox.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ExponentBox.Location = new System.Drawing.Point(35, 9);
+            this.ExponentBox.Name = "ExponentBox";
+            this.ExponentBox.Size = new System.Drawing.Size(133, 16);
+            this.ExponentBox.TabIndex = 3;
+            this.ExponentBox.Text = "1";
+            this.ExponentBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ExponentBox_KeyPress);
+            // 
+            // ProgressStatusBar
+            // 
+            this.ProgressStatusBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ProgressStatusBar.Location = new System.Drawing.Point(12, 74);
+            this.ProgressStatusBar.Name = "ProgressStatusBar";
+            this.ProgressStatusBar.Size = new System.Drawing.Size(535, 23);
+            this.ProgressStatusBar.TabIndex = 4;
+            // 
+            // TimeLabel
+            // 
+            this.TimeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.TimeLabel.AutoSize = true;
+            this.TimeLabel.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TimeLabel.Location = new System.Drawing.Point(185, 9);
+            this.TimeLabel.Name = "TimeLabel";
+            this.TimeLabel.Size = new System.Drawing.Size(362, 48);
+            this.TimeLabel.TabIndex = 5;
+            this.TimeLabel.Text = "00:00:00.0000000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(151, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "桁";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 109);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TimeLabel);
+            this.Controls.Add(this.ProgressStatusBar);
+            this.Controls.Add(this.ExponentBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "PI";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Timer Ticker;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ExponentBox;
+        private System.Windows.Forms.ProgressBar ProgressStatusBar;
+        private System.Windows.Forms.Label TimeLabel;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PI/MainForm.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace PI
+{
+    public partial class MainForm : Form
+    {
+        FileStream file;
+
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        private Task task;
+        private bool running;
+
+        private BigInteger index;
+        private BigInteger limit;
+        private BigInteger digit;
+        private BigInteger digit2;
+        private BigInteger a;
+        private BigInteger b;
+        private BigInteger t;
+        private BigInteger p;
+
+        private static BigInteger two = new BigInteger(2);
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            StartButton.Enabled = false;
+            StopButton.Enabled = true;
+            Ticker.Enabled = true;
+
+            index = BigInteger.Zero;
+            limit = BigInteger.Zero;
+
+            if (task != null)
+            {
+                task.Dispose();
+            }
+
+            task = new Task(() =>
+            {
+                try
+                {
+                    file = File.Open("PI.txt", FileMode.Create, FileAccess.Write, FileShare.Read);
+
+                    limit = BigInteger.Parse(ExponentBox.Text);
+                    digit = new BigInteger(10).Pow(two.Pow(limit + 1));
+                    digit2 = digit * digit;
+
+                    a = digit;
+                    b = digit2 / (two * digit2).Sqrt();
+                    t = digit / new BigInteger(4);
+                    p = digit;
+
+                    BigInteger an = BigInteger.Zero;
+                    BigInteger bn = BigInteger.Zero;
+                    BigInteger tn = BigInteger.Zero;
+                    BigInteger pn = BigInteger.Zero;
+
+                    string pi = string.Empty;
+
+                    BigInteger n = BigInteger.Zero;
+
+                    for (index = BigInteger.Zero; index < limit && running; index++)
+                    {
+                        an = (a + b) / two;
+                        bn = (a * b).Sqrt();
+                        tn = t- BigInteger.Pow(a - an, 2) * p / (digit2);
+                        pn = p * two;
+
+                        a = an;
+                        b = bn;
+                        t = tn;
+                        p = pn;
+
+                        pi = (BigInteger.Pow(a + b, 2) / (t * 4)).ToString();
+                        n = two.Pow(index + 1);
+                        if (pi.Length > 1 && n < int.MaxValue)
+                        {
+                            pi = pi.Substring(0, (int)n);
+                        }
+                        pi = pi.Insert(1, ".");
+                        byte[] vs = Encoding.UTF8.GetBytes($"{pi}\n");
+                        file.Write(vs, 0, vs.Length);
+                        file.Flush();
+                    }
+
+                    if (running)
+                    {
+                        Invoke(new Action(() =>
+                        {
+                            StartButton.Enabled = true;
+                            StopButton.Enabled = false;
+                            Ticker.Enabled = false;
+
+                            sw.Stop();
+                            running = false;
+
+                            file.Close();
+
+                            TimeSpan ts = sw.Elapsed;
+                            MessageBox.Show(this, $"計算にかかった時間は{(ts.Hours == 0 ? string.Empty : $"{ ts.Hours}時間")}{(ts.Minutes == 0 ? string.Empty : $"{ts.Minutes}分")}{(ts.Seconds == 0 ? string.Empty : $"{ts.Seconds}秒")}{ts.Milliseconds}ミリ秒です。\n最終的な円周率の近似値は{pi}です。", "計算が完了しました！", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            TimeLabel.Text = "00:00:00.0000000";
+                            ProgressStatusBar.Value = 0;
+                        }));
+                    }
+                }
+                catch (Exception ee)
+                {
+                    Invoke(new Action(()=>
+                    {
+                        ShowException(ee);
+                    }));
+                }
+            });
+
+            sw.Restart();
+            running = true;
+            task.Start();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            StartButton.Enabled = true;
+            StopButton.Enabled = false;
+            Ticker.Enabled = false;
+
+            sw.Stop();
+            running = false;
+            task.Wait();
+            task.Dispose();
+
+            file.Close();
+
+            TimeLabel.Text = "00:00:00.0000000";
+            ProgressStatusBar.Value = 0;
+        }
+
+        private static BigInteger onehundred = new BigInteger(100);
+
+        private void Ticker_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                TimeLabel.Text = sw.Elapsed.ToString();
+                ProgressStatusBar.Value = (int)(index * onehundred / limit);
+            }
+            catch (Exception ee)
+            {
+                ShowException(ee);
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ee)
+            {
+                ShowException(ee);
+            }
+            /*File.OpenRead("PI_Result.txt");
+            File.OpenRead("PI_Variable.txt");*/
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (task != null)
+            {
+                task.Dispose();
+            }
+
+            if (file != null)
+            {
+                file.Close();
+            }
+        }
+
+        private void ShowException(Exception e)
+        {
+            MessageBox.Show($"{e.Source}\n{e.Source}\n{e.StackTrace}", e.Message, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void ExponentBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!"0123456789\b".Contains(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+
+    static class Expand
+    {
+        public static BigInteger Sqrt(this BigInteger value)
+        {
+            BigInteger result = BigInteger.One;
+            BigInteger temp = BigInteger.Zero;
+            while (result != temp)
+            {
+                temp = result;
+                result = (result + value / result) >> 1;
+            }
+            return result;
+        }
+
+        public static BigInteger Pow(this BigInteger value, BigInteger exponent)
+        {
+            BigInteger result = BigInteger.One;
+            for (BigInteger i=BigInteger.Zero; i<exponent; i++)
+            {
+                result *= value;
+            }
+            return result;
+        }
+    }
+}
